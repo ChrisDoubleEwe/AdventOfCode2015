9e9a1e5d1161aa0896ab624ad71d79528463628d
Chris Walsh <chris@offwhitehouse.co.uk>
2021-02-04 08:18:37 +0000

    Day 14

---
+++ 14_b.py
@@ -0,0 +1,74 @@
+import sys
+from ctypes import *
+import copy
+import hashlib
+import itertools
+import re
+from printf import printf
+
+with open("14_input.txt") as f:
+    content = f.readlines()
+
+data = []
+deer = []
+
+for c in content:
+  match = re.search('([a-zA-Z]*) can fly ([0-9]*) .* for ([0-9]*) seconds, but then must rest for ([0-9]*) seconds.', c)
+
+  if match:
+    deer.append(match.group(1))
+    d = []
+    d.append(match.group(1))       # 0 - Name
+    d.append(int(match.group(2)))  # 1 - dist
+    d.append(int(match.group(3)))  # 2 - fly time
+    d.append(int(match.group(4)))  # 3 - rest time
+    d.append(1)                    # 4 - 1= flying ; 0 = rest
+    d.append(int(match.group(3)))  # 5 - timer
+    d.append(0)                    # 6 - total dist
+    d.append(0)                    # 7 - score
+
+
+
+
+    data.append(copy.deepcopy(d))
+
+   
+
+for sec in range(0, 2504):
+  for z in range(0, len(deer)):
+    i = data[z]
+    if i[4] == 1:
+      i[6] += i[1]
+      i[5] += -1
+      if i[5] == 0:
+        i[4] = 0
+        i[5] = i[3]
+        continue
+    else:
+      i[5] += -1
+      if i[5] == 0:
+        i[4] = 1
+        i[5] = i[2]
+  max_dist = 0
+  for z in range(0, len(deer)):
+    i = data[z]
+    if i[6] > max_dist:
+      max_dist = i[6]
+  for z in range(0, len(deer)):
+    i = data[z]
+    if i[6] == max_dist:
+      data[z][7]+=1
+     
+
+
+
+
+
+max = 0
+max_p = []
+for p in data:
+  if p[7] > max:
+    max = p[7]
+    max_p = p
+
+print "Part B: " + str(max_p[7])
